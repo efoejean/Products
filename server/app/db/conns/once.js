0982f1e1069d23f82f59709c19e368b5456e2a6e
efoejean <efoejean@gmail.com>
2021-10-13 21:51:43 -0500

    Delete data before sending

---
+++ server/app/db/conns/once.js
@@ -0,0 +1,15 @@
+// Loads a reusable Mongo client for the application.
+import { MongoClient } from "mongodb";
+import config from "../../config.js";
+
+const client = new MongoClient(config.db);
+
+export default {
+  connect() {
+    // return a promise
+    return client.connect();
+  },
+  close() {
+    client.close();
+  },
+};
--- server/app/db/seeds.js
+++ server/app/db/seeds.js
@@ -1,13 +1,11 @@
 import productsData from "./data.js";
 import once from "./conns/once.js";
 
-// Create a databases with the connection from seeds with mongoaltas.
-//
-once
-  .connect()
-  .then((connection) =>
-    connection.db("products").collection("products").insertMany(productsData)
-  )
-  .then(() => {
-    once.close();
-  });
+// Asynchronous Anonymous IIFE - Immediately Invoked Function Expression.
+
+(async () => {
+  const conn = await once.connect();
+  await conn.db("products").collection("products").deleteMany({});
+  await conn.db("products").collection("products").insertMany(productsData);
+  conn.close();
+})();
