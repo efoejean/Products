b10c68fab8cc99e052be4c53834232693f6d0961
efoejean <77749280+efoejean@users.noreply.github.com>
2021-10-27 21:50:33 -0500

    enable corse

--- server/app/index.js
+++ server/app/index.js
@@ -3,6 +3,9 @@
 import express from "express";
 import config from "./config.js";
 import router from "./router.js";
+
+import cors from "cors";
+
 // TODO: Import the routes
 
 const app = express();
@@ -12,6 +15,10 @@ app.get("/", (_, res) => {
 });
 
 app.use(express.json());
+
+
+
+app.use(cors())
 
 app.use("/api", router);
 
